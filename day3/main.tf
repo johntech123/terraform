272b6bc777c28eb4eca0b8b56838354d45985d98
shahrukh <sk.shahrukh0123@gmail.com>
2025-05-12 14:39:18 +0530

    ssh-connection-ec2-instance

--- day3/main.tf
+++ day3/main.tf
@@ -7,3 +7,13 @@ resource "aws_key_pair" "key_tf" {
   public_key = file("${path.module}/id_rsa.pub")
 }
 
+output "key_name" {
+  value = aws_key_pair.key_tf.key_name
+}
+
+resource "aws_instance" "name" {
+  ami = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.key_tf.key_name
+}
+
